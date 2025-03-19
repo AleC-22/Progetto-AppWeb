5d4474d1baa09b5f8d96e5cf9267a02d498f3f9d
alessio contartese <alessiocontartese@gmail.com>
2025-03-19 21:48:14 +0100

    SignOut

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,4 +1,19 @@
+import {getAuth, signOut} from "firebase/auth";
+
+
+
 export function HomePage({setUser}){
+    const auth = getAuth();
+
+    const signOutHandler = async () => {
+            await signOut(auth)
+                .then(() => {
+                    setUser(null);
+                    console.log("Utente disconnesso con successo!")
+                }).catch(error => {
+                    console.log(error)
+                });
+    };
 
     return(
         <>
@@ -6,11 +21,9 @@ export function HomePage({setUser}){
                 <div className="sidebar">
                     <a className="active" href="#">Home</a>
                     <a href="#">News</a>
-                    <a href="#">Contact</a>
-                    <a href="#">About</a>
                     <a href={"#"}>Profilo</a>
                     <div className={"logout-box"}>
-                        <a href={"#"} onClick={()=>setUser(null)}>
+                        <a href={"#"} onClick={(signOutHandler)}>
                             Logout
                         </a>
                     </div>
